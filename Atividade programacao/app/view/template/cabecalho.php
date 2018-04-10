fbbdbbb242e3228979e35145db3667a858b5ae44
HugoGutzmann <facahugo@gmail.com>
2018-04-09 21:27:16 -0300

    Add files via upload

---
+++ Atividade programacao/app/view/template/cabecalho.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="../style/style.css">
+<style>
+
+
+</style>
+
+<div class="cabecalho">
+    <h1>Lista de Categorias</h1>
+</div>
---
+++ Atividade programacao/app/view/template/rodape.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="../style/style.css">
+
+<div class="rodape">
+  <p>Hugo Puga 3info1</p>
+</div>
